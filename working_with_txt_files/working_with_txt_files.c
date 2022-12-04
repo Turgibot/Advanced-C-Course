b129b85549c41952c21f0611a239f4f41ff13cee
Guy Tordjman <turgibot@gmail.com>
2022-12-04 13:31:11 +0200

    finis part 1

--- working_with_txt_files/working_with_txt_files.c
+++ working_with_txt_files/working_with_txt_files.c
@@ -3,18 +3,20 @@
 #include <stdlib.h>
 #include <string.h>
 void example();
-
+void useFEOF();
+void copyFileExample();
 
 void main() {
-    example();
-  
+    //example();
+    //useFEOF();
+    copyFileExample();
 }
 
 void example() {
     char str[80];   
     float f;   
     FILE* pFile;
-    pFile = fopen("myfile.txt", "w+");
+    pFile = fopen("myData.txt", "w+");
     if (pFile == NULL) exit(1);
 
     fprintf(pFile, "%f%s", 3.1416, "PI");
@@ -23,5 +25,51 @@ void example() {
     fscanf(pFile, "%s", str);
     fclose(pFile);
     printf("I have read: %f and %s \n", f, str);
+
+}
+
+void useFEOF() {
+    FILE* pFile;
+    char buffer[80];
+    pFile = fopen("myfile.txt", "r");
+    if (pFile == NULL)
+        fprintf(stdout, "Error opening file"); //screen
+    else
+    {
+        while (!feof(pFile))
+            if (fgets(buffer, 80, pFile) != NULL)
+                fprintf(stdout, "%s", buffer); //screen
+
+        if (fclose(pFile))
+            fprintf(stdout, "Error in closing file");
+    }
+
+}
+
+void copyFileExample() {
+    int ch; 
+    FILE* fsource = fopen("myfile.txt", "rt");  //open read only
+    FILE *fdestination = fopen("mycopy.txt","wt+");//open – write + read  
+    if (!fsource || !fdestination) {
+        puts("Unable to open file...\n"); exit(1);
+    }
+    do {
+        ch = fgetc(fsource); //read 1 char 
+        if  (ch!=' ' && ch!=EOF) 
+            fputc(ch, fdestination);//not ‘ ‘ nor EOF
+    } while (ch != EOF);
+
+    fclose(fsource);	 //close file 
+
+    // Print the new file to screen
+    fseek(fdestination, 0, SEEK_SET);
+
+    while((ch = fgetc(fdestination)) != EOF) //read one char
+        putchar(ch);// display char
+
+    putchar('\n');
+
+    fclose(fdestination);//close file 
+    printf("End of program...\n");
 
 }
